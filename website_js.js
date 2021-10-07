d9e4ecbb4c190c101a42bbb341d983f5a1dd7383
vka001 <vineetagrawal001@gmail.com>
2021-10-07 14:55:25 +0530

    Update website_js.js

--- website_js.js
+++ website_js.js
@@ -1,5 +1,34 @@
-var dt = new Date();
-document.getElementById('date-time').innerHTML=dt;
+// var dt = new Date();
+// document.getElementById('date-time').innerHTML=dt;
+
+function covertMonth(num){
+  let months = ['Jan','Feb','Mar','Apr','May','Jun','Jul','Aug','Sep','Oct','Nov','Dec'];
+  // look into index with num 0-11
+  let computedRes = months[num];
+  return computedRes;
+}
+
+// time func
+function Time(){
+   // important to get new instant of the Date referrance
+  let date = new Date();
+  this.time = date.toLocaleTimeString();
+  this.year = date.getUTCFullYear();
+  this.day = date.getUTCDate();
+  this.month = date.getUTCMonth();
+  this.currentTime = date.toLocaleTimeString() + ' ' + covertMonth(this.month) + ' ' + this.day + ' ' + this.year;
+  return this.currentTime;
+}
+
+
+ function timeOutPut(){
+  let where = document.getElementById('date-time');
+  where.textContent = Time(); // 1:21:39 AM Dec 17 2017
+}
+
+   // run every 5secs
+setInterval(timeOutPut, 1000);
+
 
 function home(){
     document.getElementById("seconddiv").innerHTML=`<div >
@@ -17,7 +46,7 @@ function home(){
 function abt(){
   document.getElementById("seconddiv").innerHTML=`<div class="homepage" >
   <h1 >ABOUT</h1>
-  <p class="about">I'm an optimistic individual who has a positive outlook on life.I have certain traits which acquires a good sense of humour.<br>
+  <p class="about">I'm an optimistic individual who has a positive outlook on life.I have certain traits which acquires a good sense of humour.
       Currently I am presuing Masters in Technology (M.Tech) in Computer Science and Engineering ( 2021 - 2023 ),from one of India's best Institute <a href="https://www.iiit.ac.in/" class="links" target="_blank">International Institute of Technology , Hyderabad (IIIT,Hyderabad)</a>
       My area of interest is Data Structure and Algorithm.I have completed my Bachelors in Technology (B.Tech) in Computer Science and Engineering ( 2016 - 2020 ),from <a href="https://www.heritageit.edu/" class="links" target="_blank">Heritage Institute of Technology , Kolkata (HIT,Kolkata)</a>.I learnt many things and made lot of memories during my B.Tech and now looking forward to this new phase of learning.
       I am from the "Coal Capital of India" Dhanbad,Jharkhand.And I have completed my schooling that is Secondary Education( 2013 - 2014 ) and Senior Secondary Education( 2014 - 2016 ) both from <a href="http://davkoylanagar.com/" class="links" target="_blank">D.A.V. Public School,Koyla Nagar,Dhanbad</a>.I love listening to songs and I am a very good Table Tennis player.My current love is Chess and I am really very passionate about it.I refer myself as an "end pawn" which means currently I am a pawn but one day this pawn can become anything he wants.
@@ -65,6 +94,16 @@ function abt(){
 
 function wins(){
   document.getElementById("seconddiv").innerHTML=`<div class="homepage">
+  <h1 >PROJECTS</h1>
+  <br>
+  <h4>Ease-O-Click: Making lives easier with a click!</h4>
+  <p>
+  The platform is a one step solution to all the problems Indian mothers and working
+  couples face with repairment of Electrical and Electronic items used in houses today.
+  It connects the entire community of technicians and customers with just one click.Major technologies used for building this project is MySQL
+  ,HTML,CSS ,Bootstrap and PHP for backend. The main motive of this project is to bring technicians online and give them better work opportunities and also 
+  to support mothers especially the homemakers to get the things done without going out in search of technicians.
+  </p><br><br>
   <h1 >WINS</h1>
   <h4>"A Little Progress Each Day Adds Up To Big Results." </h4>
   <p>I have secured an AIR of 328 in Graduate Aptitude Test in Engineering(CSE) 2021.</p> 
@@ -84,9 +123,9 @@ function contact(){
   <h1>CONTACT</h1>
   <img class="location" src="https://raw.githubusercontent.com/vka001/pic/main/location.png" alt="location";>
   Mobile No: +917209293856<br>
-  <br><a href="https://github.com/vka001" class="links">
+  <br><a href="https://github.com/vka001" class="links" target="_blank">
   <i class="fa fa-github"></i> GITHUB</a> <br> 
-  <a href="https://www.linkedin.com/in/vineet-agrawal-503a03152" class="links">
+  <a href="https://www.linkedin.com/in/vineet-agrawal-503a03152" class="links" target="_blank">
   <i class="fa fa-linkedin-square"></i> Linkedin</a>
 </div>`;
 }
